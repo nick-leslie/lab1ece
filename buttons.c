053e657af530f5ebd723956155b13797202ed36f
Nick Leslie <46449945+nick-leslie@users.noreply.github.com>
2025-04-04 19:28:18 -0400

    Update buttons.c

--- buttons.c
+++ buttons.c
@@ -1,107 +1,196 @@
-/**
- * main.c
+/*
+ * buttons.c
  *
- * ECE 3849 Lab 0 Starter Project
- * Gene Bogdanov    10/18/2017
+ *  Created on: Aug 12, 2012, modified 9/8/2017
+ *      Author: Gene Bogdanov
  *
- * This version is using the new hardware for B2017: the EK-TM4C1294XL LaunchPad with BOOSTXL-EDUMKII BoosterPack.
- *
+ * ECE 3849 Lab button handling
  */
-
 #include <stdint.h>
 #include <stdbool.h>
-#include "driverlib/fpu.h"
-#include "driverlib/sysctl.h"
-#include "driverlib/interrupt.h"
-#include "Crystalfontz128x128_ST7735.h"
-#include "buttons.h"
-#include <stdio.h>
 #include <math.h>
 #include "inc/hw_memmap.h"
+#include "inc/hw_ints.h"
+#include "driverlib/sysctl.h"
 #include "driverlib/gpio.h"
-#include "driverlib/pwm.h"
-#include "driverlib/pin_map.h"
+#include "driverlib/timer.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/adc.h"
+#include "sysctl_pll.h"
+#include "Crystalfontz128x128_ST7735.h"
+#include "buttons.h"
 #include "sampling.h"
-#define PWM_FREQUENCY 20000 // PWM frequency = 20 kHz
 
+// public globals
+volatile uint32_t gButtons = 0; // debounced button state, one per bit in the lowest bits
+                                // button is pressed if its bit is 1, not pressed if 0
+uint32_t gJoystick[2] = {0};    // joystick coordinates
+uint32_t gADCSamplingRate;      // [Hz] actual ADC sampling rate
+// imported globals
+extern uint32_t gSystemClock;   // [Hz] system clock frequency
+extern volatile uint32_t gTime; // time in hundredths of a second
 
-uint32_t gSystemClock; // [Hz] system clock frequency
-volatile uint32_t gTime = 8345; // time in hundredths of a second
+extern volatile bool should_update_draw_buffer;
 
-volatile bool should_update_draw_buffer = true;
-
-
-void signal_init();
-
-int main(void)
+// initialize all button and joystick handling hardware
+void ButtonInit(void)
 {
-    IntMasterDisable();
+    // initialize a general purpose timer for periodic interrupts
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+    TimerDisable(TIMER0_BASE, TIMER_BOTH);
+    TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);
+    TimerLoadSet(TIMER0_BASE, TIMER_A, roundf((float)gSystemClock / BUTTON_SCAN_RATE) - 1);
+    TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+    TimerEnable(TIMER0_BASE, TIMER_BOTH);
 
-    // Enable the Floating Point Unit, and permit ISRs to use it
-    FPUEnable();
-    FPULazyStackingEnable();
+    // initialize interrupt controller to respond to timer interrupts
+    IntPrioritySet(INT_TIMER0A, BUTTON_INT_PRIORITY);
+    IntEnable(INT_TIMER0A);
 
-    // Initialize the system clock to 120 MHz
-    gSystemClock = SysCtlClockFreqSet(SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480, 120000000);
+    // GPIO PJ0 and PJ1 = EK-TM4C1294XL buttons 1 and 2
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOJ);
+    GPIOPinTypeGPIOInput(GPIO_PORTJ_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+    GPIOPadConfigSet(GPIO_PORTJ_BASE, GPIO_PIN_0 | GPIO_PIN_1 , GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
 
-    Crystalfontz128x128_Init(); // Initialize the LCD display driver
-    Crystalfontz128x128_SetOrientation(LCD_ORIENTATION_UP); // set screen orientation
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOH);
+    GPIOPinTypeGPIOInput(GPIO_PORTH_BASE, GPIO_PIN_1);
+    GPIOPadConfigSet(GPIO_PORTH_BASE,  GPIO_PIN_1, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
 
-    tContext sContext;
-    GrContextInit(&sContext, &g_sCrystalfontz128x128); // Initialize the grlib graphics context
-    GrContextFontSet(&sContext, &g_sFontFixed6x8); // select font
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOK);
+    GPIOPinTypeGPIOInput(GPIO_PORTK_BASE, GPIO_PIN_6);
+    GPIOPadConfigSet(GPIO_PORTK_BASE,  GPIO_PIN_6, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
 
-//    uint32_t time;  // local copy of gTime
-//    char str[50];   // string buffer
-    // full-screen rectangle
-    uint16_t max_width = GrContextDpyWidthGet(&sContext)-1;
-    uint16_t max_height =  GrContextDpyHeightGet(&sContext)-1;
-    tRectangle rectFullScreen = {0, 0, max_width,max_height};
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
+    GPIOPinTypeGPIOInput(GPIO_PORTD_BASE, GPIO_PIN_4 );
+    GPIOPadConfigSet(GPIO_PORTD_BASE,  GPIO_PIN_4, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
 
-    //start icr
-    ButtonInit();
-    signal_init();
-    sample_init();
-    IntMasterEnable();
+    // analog input AIN13, at GPIO PD2 = BoosterPack Joystick HOR(X)
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
+    GPIOPinTypeADC(GPIO_PORTD_BASE, GPIO_PIN_2);
+    // analog input AIN17, at GPIO PK1 = BoosterPack Joystick VER(Y)
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOK);
+    GPIOPinTypeADC(GPIO_PORTK_BASE, GPIO_PIN_1);
 
+    // initialize ADC0 peripheral
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
+    uint32_t pll_frequency = SysCtlFrequencyGet(CRYSTAL_FREQUENCY);
+    uint32_t pll_divisor = (pll_frequency - 1) / (16 * ADC_SAMPLING_RATE) + 1; // round divisor up
+    gADCSamplingRate = pll_frequency / (16 * pll_divisor); // actual sampling rate may differ from ADC_SAMPLING_RATE
+    ADCClockConfigSet(ADC0_BASE, ADC_CLOCK_SRC_PLL | ADC_CLOCK_RATE_FULL, pll_divisor); // only ADC0 has PLL clock divisor control
 
+    // initialize ADC sampling sequence
+    ADCSequenceDisable(ADC0_BASE, 0);
+    ADCSequenceConfigure(ADC0_BASE, 0, ADC_TRIGGER_PROCESSOR, 0);
+    ADCSequenceStepConfigure(ADC0_BASE, 0, 0, ADC_CTL_CH13);                             // Joystick HOR(X)
+    ADCSequenceStepConfigure(ADC0_BASE, 0, 1, ADC_CTL_CH17 | ADC_CTL_IE | ADC_CTL_END);  // Joystick VER(Y)
+    ADCSequenceEnable(ADC0_BASE, 0);
 
-    while (true) {
-        draw_grid(&sContext,&rectFullScreen,max_width,max_height);
-        if(should_update_draw_buffer ==true) {
-            update_draw_buffer();
-            should_update_draw_buffer=false;
-        }
-//        GrContextForegroundSet(&sContext, ClrBlack);
-//        GrRectFill(&sContext, &rectFullScreen); // fill screen with black
-//        time = gTime; // read shared global only once
-//        int minutes = time / 6000;
-//        int remaining = time % 6000;
-//        int seconds = remaining / 100;
-//        int hundreds_of_seconds = remaining % 100;
-//        snprintf(str, sizeof(str), "Time = %d:%d:%d", minutes,seconds,hundreds_of_seconds); // convert time to string
-//        GrContextForegroundSet(&sContext, ClrYellow); // yellow text
-//        GrStringDraw(&sContext, str, /*length*/ -1, /*x*/ 0, /*y*/ 0, /*opaque*/ false);
-//        GrFlush(&sContext); // flush the frame buffer to the LCD
-    }
 }
 
-void signal_init() {
-    // configure M0PWM2, at GPIO PF2, BoosterPack 1 header C1 pin 2
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-    GPIOPinTypePWM(GPIO_PORTF_BASE, GPIO_PIN_2);
-    GPIOPinConfigure(GPIO_PF2_M0PWM2);
-    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_2,
-    GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD);
-    // configure the PWM0 peripheral, gen 1, outputs 2 and 3
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM0);
-    // use system clock without division
-    PWMClockSet(PWM0_BASE, PWM_SYSCLK_DIV_1);
-    PWMGenConfigure(PWM0_BASE, PWM_GEN_1,
-    PWM_GEN_MODE_DOWN | PWM_GEN_MODE_NO_SYNC);
-    PWMGenPeriodSet(PWM0_BASE, PWM_GEN_1,roundf((float)gSystemClock/PWM_FREQUENCY));
-    PWMPulseWidthSet(PWM0_BASE, PWM_OUT_2,
-    roundf((float)gSystemClock/PWM_FREQUENCY*0.4f));
-    PWMOutputState(PWM0_BASE, PWM_OUT_2_BIT, true);
-    PWMGenEnable(PWM0_BASE, PWM_GEN_1);
+// update the debounced button state gButtons
+void ButtonDebounce(uint32_t buttons)
+{
+	int32_t i, mask;
+	static int32_t state[BUTTON_COUNT]; // button state: 0 = released
+									    // BUTTON_PRESSED_STATE = pressed
+									    // in between = previous state
+	for (i = 0; i < BUTTON_COUNT; i++) {
+		mask = 1 << i;
+		if (buttons & mask) {
+			state[i] += BUTTON_STATE_INCREMENT;
+			if (state[i] >= BUTTON_PRESSED_STATE) {
+				state[i] = BUTTON_PRESSED_STATE;
+				gButtons |= mask; // update debounced button state
+			}
+		}
+		else {
+			state[i] -= BUTTON_STATE_DECREMENT;
+			if (state[i] <= 0) {
+				state[i] = 0;
+				gButtons &= ~mask;
+			}
+		}
+	}
+}
+
+// sample joystick and convert to button presses
+void ButtonReadJoystick(void)
+{
+    ADCProcessorTrigger(ADC0_BASE, 0);          // trigger the ADC sample sequence for Joystick X and Y
+    while(!ADCIntStatus(ADC0_BASE, 0, false));  // wait until the sample sequence has completed
+    ADCSequenceDataGet(ADC0_BASE, 0, gJoystick);// retrieve joystick data
+    ADCIntClear(ADC0_BASE, 0);                  // clear ADC sequence interrupt flag
+
+    // process joystick movements as button presses using hysteresis
+    if (gJoystick[0] > JOYSTICK_UPPER_PRESS_THRESHOLD) gButtons |= 1 << 5; // joystick right in position 5
+    if (gJoystick[0] < JOYSTICK_UPPER_RELEASE_THRESHOLD) gButtons &= ~(1 << 5);
+
+    if (gJoystick[0] < JOYSTICK_LOWER_PRESS_THRESHOLD) gButtons |= 1 << 6; // joystick left in position 6
+    if (gJoystick[0] > JOYSTICK_LOWER_RELEASE_THRESHOLD) gButtons &= ~(1 << 6);
+
+    if (gJoystick[1] > JOYSTICK_UPPER_PRESS_THRESHOLD) gButtons |= 1 << 7; // joystick up in position 7
+    if (gJoystick[1] < JOYSTICK_UPPER_RELEASE_THRESHOLD) gButtons &= ~(1 << 7);
+
+    if (gJoystick[1] < JOYSTICK_LOWER_PRESS_THRESHOLD) gButtons |= 1 << 8; // joystick down in position 8
+    if (gJoystick[1] > JOYSTICK_LOWER_RELEASE_THRESHOLD) gButtons &= ~(1 << 8);
+}
+
+// autorepeat button presses if a button is held long enough
+uint32_t ButtonAutoRepeat(void)
+{
+    static int count[BUTTON_AND_JOYSTICK_COUNT] = {0}; // autorepeat counts
+    int i;
+    uint32_t mask;
+    uint32_t presses = 0;
+    for (i = 0; i < BUTTON_AND_JOYSTICK_COUNT; i++) {
+        mask = 1 << i;
+        if (gButtons & mask)
+            count[i]++;     // increment count if button is held
+        else
+            count[i] = 0;   // reset count if button is let go
+        if (count[i] >= BUTTON_AUTOREPEAT_INITIAL &&
+                (count[i] - BUTTON_AUTOREPEAT_INITIAL) % BUTTON_AUTOREPEAT_NEXT == 0)
+            presses |= mask;    // register a button press due to auto-repeat
+    }
+    return presses;
+}
+
+// ISR for scanning and debouncing buttons
+void ButtonISR(void) {
+    TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT); // clear interrupt flag
+    // read hardware button state
+    uint32_t gpio_buttons =
+            ~GPIOPinRead(GPIO_PORTJ_BASE, 0xff) & (GPIO_PIN_1 | GPIO_PIN_0) // EK-TM4C1294XL buttons in positions 0 and 1
+            | ((~GPIOPinRead(GPIO_PORTH_BASE, 0xff) & (GPIO_PIN_1)) << 1) // s1
+            | ((~GPIOPinRead(GPIO_PORTK_BASE, 0xff) & (GPIO_PIN_6)) >> 3) // s2
+            | ((~GPIOPinRead(GPIO_PORTD_BASE, 0xff) & (GPIO_PIN_4))) // select
+
+            ;
+
+
+
+    uint32_t old_buttons = gButtons;    // save previous button state
+    ButtonDebounce(gpio_buttons);       // Run the button debouncer. The result is in gButtons.
+    ButtonReadJoystick();               // Convert joystick state to button presses. The result is in gButtons.
+    uint32_t presses = ~old_buttons & gButtons;   // detect button presses (transitions from not pressed to pressed)
+    presses |= ButtonAutoRepeat();      // autorepeat presses if a button is held long enough
+
+    static bool tic = false;
+    static bool running = true;
+
+    if (presses & GPIO_PIN_0) { // EK-TM4C1294XL button 1 pressed
+        running = !running;
+    }
+    if(presses & GPIO_PIN_1) {
+        gTime=0;
+    }
+
+    if (presses & 0x04) {
+        should_update_draw_buffer=true;
+    }
+
+    if (running) {
+        if (tic) gTime++; // increment time every other ISR call
+        tic = !tic;
+    }
 }
